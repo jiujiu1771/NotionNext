6d7124c3f2acedfa9caf29e0414c2d60a8b42a93
jiujiu1771 <136544585+jiujiu1771@users.noreply.github.com>
2023-11-24 10:46:02 +0800

    Update Announcement.js

--- themes/hexo/components/Announcement.js
+++ themes/hexo/components/Announcement.js
@@ -1,12 +1,7 @@
-import { useGlobal } from '@/lib/global'
-import dynamic from 'next/dynamic'
-
-const NotionPage = dynamic(() => import('@/components/NotionPage'))
-
 const Announcement = ({ post, className }) => {
   const { locale } = useGlobal()
   if (post?.blockMap) {
-    return <div className={className}>
+    return <div className={`${className} fixed top-0 w-full z-50`}>
         <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">
             <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
             {post && (<div id="announcement-content">
